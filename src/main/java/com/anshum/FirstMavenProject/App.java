b99a0f8e7145fd11bf0b2a086b1b744572849bdc
Anshum Gupta <anshum_it_is@MacBook-Pro.local>
2020-10-30 15:58:46 +0530

    added spring framework dependencies to pom.xml

---
+++ src/main/java/com/anshum/FirstMavenProject/App.java
@@ -0,0 +1,13 @@
+package com.anshum.FirstMavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
