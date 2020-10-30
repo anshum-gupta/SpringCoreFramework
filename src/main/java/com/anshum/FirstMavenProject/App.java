d8353acbe2ce28d94a5c9063438ee52c3d253e43
Anshum Gupta <anshum_it_is@MacBook-Pro.local>
2020-10-30 17:25:28 +0530

    taking context from xml file now

--- src/main/java/com/anshum/FirstMavenProject/App.java
+++ src/main/java/com/anshum/FirstMavenProject/App.java
@@ -1,13 +1,16 @@
 package com.anshum.FirstMavenProject;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+	
+    public static void main( String[] args ) {
+    	
     	// creating object of class car 
     	Car car = new Car();
     	car.drive();
@@ -21,5 +24,19 @@ public class App
     	// that would do the same thing
     	Vehicle vehicle = new Car();
     	vehicle.drive();
+    	
+    	
+    	// rather than changing the source code
+    	// why don't we get the context from an xml file
+    	// in that xml file, we can specify if it is a car or a bike
+    	// the advantage is that we don't have to modify source code
+    	// so we don't have to recompile it
+    	
+    	ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+    	
+    	Vehicle vehicleObject = (Vehicle) context.getBean("vehicle");
+    	vehicleObject.drive();
+    	
     }
+    
 }
